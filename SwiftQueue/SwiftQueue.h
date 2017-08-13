333f0faeb362134c9e4815d43fd4bfe9f020a464
Lucas Nelaupe <lucas.nelaupe@vcube.co.jp>
2017-08-13 16:48:57 +0800

    Revert "Remove"
    This reverts commit 1d6917e994343de3f03cf3aac0f5e5df19c21ffd.

---
+++ SwiftQueue/SwiftQueue.h
@@ -0,0 +1,19 @@
+//
+//  SwiftQueue.h
+//  SwiftQueue
+//
+//  Created by Lucas Nelaupe on 11/8/17.
+//  Copyright © 2017 Lucas Nelaupe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftQueue.
+FOUNDATION_EXPORT double SwiftQueueVersionNumber;
+
+//! Project version string for SwiftQueue.
+FOUNDATION_EXPORT const unsigned char SwiftQueueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftQueue/PublicHeader.h>
+
+
