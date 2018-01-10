4bee202835bef7c31357bfd13f06730f78abd66c
Lucas Nelaupe <lucas34@users.noreply.github.com>
2018-01-10 15:42:57 +0800

    Reduce run test time

--- Tests/SwiftQueueTests/ConstraintDeadlineTests.swift
+++ Tests/SwiftQueueTests/ConstraintDeadlineTests.swift
@@ -38,7 +38,7 @@ class ConstraintDeadlineTests: XCTestCase {
 
         let manager = SwiftQueueManager(creators: [creator])
         JobBuilder(type: type1)
-                .delay(time: 1)
+                .delay(time: 0.1)
                 .retry(max: 5)
                 .schedule(manager: manager)
 
@@ -103,7 +103,7 @@ class ConstraintDeadlineTests: XCTestCase {
         let manager = SwiftQueueManager(creators: [creator])
         JobBuilder(type: type1)
                 .delay(time: 60)
-                .deadline(date: Date(timeIntervalSinceNow: TimeInterval(2)))
+                .deadline(date: Date(timeIntervalSinceNow: TimeInterval(0.1)))
                 .retry(max: 5)
                 .schedule(manager: manager)
 
