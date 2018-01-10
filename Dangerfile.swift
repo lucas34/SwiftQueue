4eb01263b2af2e4c49e27acb3e1a00a92c8f9663
Lucas Nelaupe <lucas34@users.noreply.github.com>
2018-01-10 13:41:55 +0800

    Remove ChangeLog required from DangerFile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -14,14 +14,6 @@ if (danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.
     warn("Big PR, try to keep changes smaller if you can")
 }
 
-if !isTrivial && !changelogChanged && sourceChanges != nil {
-    warn("""
-     Any changes to library code should be reflected in the Changelog.
-
-     Please consider adding a note there and adhere to the [Changelog Guidelines](https://github.com/Moya/contributors/blob/master/Changelog%20Guidelines.md).
-    """)
-}
-
 if danger.github.pullRequest.title.contains("WIP") {
     warn("PR is classed as Work in Progress")
 }
