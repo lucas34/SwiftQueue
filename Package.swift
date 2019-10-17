68b99141168f53e531431725c408306b19ada77b
Lucas Nelaupe <7482647+lucas34@users.noreply.github.com>
2019-10-17 20:59:44 +0800

    Update Reachability to 5.0.0 (#273)

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/ashleymills/Reachability.swift", .upToNextMinor(from: "4.3.0"))
+        .package(url: "https://github.com/ashleymills/Reachability.swift", .upToNextMinor(from: "5.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
