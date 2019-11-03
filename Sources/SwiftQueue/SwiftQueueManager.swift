30800f19caa3e7383d4ca9f55e60830a7a99156d
Lucas Nelaupe <lucas.nelaupe@grabtaxi.com>
2019-11-03 19:52:40 +0800

    fix line

--- Sources/SwiftQueue/SwiftQueueManager.swift
+++ Sources/SwiftQueue/SwiftQueueManager.swift
@@ -105,7 +105,6 @@ public final class SwiftQueueManager {
         }
     }
 
-
 }
 
 /// Extension to query Job Manager
@@ -137,7 +136,6 @@ public extension SwiftQueueManager {
         return result
     }
 }
-
 
 internal extension SwiftQueueManager {
 
