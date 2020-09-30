c7c6c893ff8364a2a3ad5244a44645b35c5227f2
Lucas Nelaupe <lucas.nelaupe@grabtaxi.com>
2020-10-01 00:47:14 +0800

    Bump

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "Reachability", url: "https://github.com/ashleymills/Reachability.swift", .upToNextMinor(from: "5.0.0"))
+        .package(name: "Reachability", url: "https://github.com/ashleymills/Reachability.swift", .upToNextMinor(from: "5.1.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
