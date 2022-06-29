05e18d8878e457c9baa8ee47e767d4cf3d8b8e38
Lucas Nelaupe <>
2022-06-29 12:53:48 +0800

    Update copyright

--- Tests/SwiftQueueTests/StartStopTests.swift
+++ Tests/SwiftQueueTests/StartStopTests.swift
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2019 Lucas Nelaupe
+// Copyright (c) 2022 Lucas Nelaupe
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
