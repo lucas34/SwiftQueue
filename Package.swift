90e65851df50cdba16a2e6ed9dc3da528fec947c
lucas.nelaupe <lucas.nelaupe@bytedance.com>
2025-06-09 16:01:43 +0800

    Update Swift version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7.0
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,24 +6,21 @@ import PackageDescription
 let package = Package(
     name: "SwiftQueue",
     platforms: [
-        .macOS(.v10_14),
-        .iOS(.v12),
+        .macOS(.v11),
+        .iOS(.v13),
         .macCatalyst(.v13),
-        .tvOS(.v12),
-        .watchOS(.v5)
+        .tvOS(.v13),
+        .watchOS(.v6)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftQueue",
             targets: ["SwiftQueue"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
+        // Add external dependencies here if needed
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftQueue",
             dependencies: []),
--- SwiftQueue.podspec
+++ SwiftQueue.podspec
@@ -2,19 +2,18 @@ Pod::Spec.new do |s|
   s.name             = "SwiftQueue"
   s.version          = "6.0.2"
   s.summary          = "SwiftQueue"
-  s.description      = "Job Scheduler for IOS with Concurrent run, failure/retry, persistence, repeat, delay and more"
+  s.description      = "Job Scheduler for iOS with concurrent execution, retry/failure handling, persistence, and more."
   s.homepage         = "https://github.com/lucas34/SwiftQueue"
-  s.license          = 'MIT'
+  s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { "Lucas Nelaupe" => "lucas.nelaupe@gmail.com" }
   s.source           = { :git => "https://github.com/lucas34/SwiftQueue.git", :tag => s.version.to_s }
 
-  s.swift_version = '5.7.0'
+  s.swift_version = '5.10'
 
-  s.ios.deployment_target = "12.0"
-  s.tvos.deployment_target= "12.0"
-  s.watchos.deployment_target = "5.0"
-  s.osx.deployment_target= "10.14"
+  s.ios.deployment_target     = "13.0"
+  s.tvos.deployment_target    = "13.0"
+  s.watchos.deployment_target = "6.0"
+  s.osx.deployment_target     = "10.15"
 
-  s.source_files = 'Sources/SwiftQueue/**.swift'
-  
+  s.source_files = 'Sources/SwiftQueue/**/*.swift'
 end
